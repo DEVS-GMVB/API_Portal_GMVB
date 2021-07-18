57373b3bc69fb3c0145617807d53418984dfdc0a
marvim1990 <marcosvinim.cruz@gmail.com>
2021-07-18 12:15:34 -0300

    preparada para produção 1

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 const config = {
     host: 'database.grupogmvb.com', //18.299.217.146
-    username: 'dev', //grupogmvb
-    password: 'Dev@2021', //bruno0422LA
-    database: 'base_dev', //grupogmvb
+    username: 'grupogmvb', //grupogmvb
+    password: 'bruno0422LA', //bruno0422LA
+    database: 'grupogmvb', //grupogmvb
     dialect: 'mysql'
   }
   
